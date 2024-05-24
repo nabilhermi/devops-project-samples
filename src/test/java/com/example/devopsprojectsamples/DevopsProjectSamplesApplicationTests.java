a057b1ae5255696a1ddf04a2cdd860c0b80f550f
Your Name <nabil.hyrmi@gmail.com>
2024-05-24 09:51:16 +0000

    lol112

--- src/test/java/com/example/devopsprojectsamples/DevopsProjectSamplesApplicationTests.java
+++ src/test/java/com/example/devopsprojectsamples/DevopsProjectSamplesApplicationTests.java
@@ -1,20 +1,13 @@
 package com.example.devopsprojectsamples;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DevopsProjectSamplesApplicationTests {
 
-    @Autowired
-    private ApplicationContext applicationContext;
+	@Test
+	void contextLoads() {
+	}
 
-    @Test
-    void contextLoads() {
-        // Vérifie que l'application context se charge correctement
-        assertThat(applicationContext).isNotNull();
-    }
 }
